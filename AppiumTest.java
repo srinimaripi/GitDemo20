44ad014b7064de8c363ebf02231085433a8f6eb7
srinimaripi <srinivasmaripi_55@hotmail.com>
2020-09-30 14:51:56 +0100

    first comit

---
+++ AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+@Test
+	
+	public void nativeAppAndroid() {
+	
+	System.out.println("nativeAndroidApp");
+		
+	}
+	
+	@Test
+	
+	public void iOSApp() {
+		System.out.println("iOSApp");
+		
+	}
+
+}
